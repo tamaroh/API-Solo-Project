60babd5d5697e9834c32b060a30f0ede4ee8b3a1
tamaroh <tamaroh@gmail.com>
2022-08-17 19:58:11 +0900

    passed test if category table exists

--- test/knex.spec.js
+++ test/knex.spec.js
@@ -1,11 +1,17 @@
-const {expect, assert} = require('chai');
-const config = require('../knexfile');
-const knex = require('knex')(config);
+const { expect, assert } = require("chai");
+const config = require("../knexfile");
+const knex = require("knex")(config);
 
-describe('server setup', () => {
-    it("should connect to database", () => {
-        knex.raw("select 1 as result").catch(() => {
-          assert.fail("unable to connect to database");
-        });
-  })
-})
+describe("server setup", () => {
+  it("should connect to database", () => {
+    knex.raw("select 1 as result").catch(() => {
+      assert.fail("unable to connect to database");
+    });
+  });
+  it("has run the initial migration", () => {
+    knex("category")
+      .select()
+      .catch(() => assert.fail("category table is not found."));
+  });
+
+});
